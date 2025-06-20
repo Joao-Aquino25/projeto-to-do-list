9141fbcb1e307d25da0af1efeebcf2b4d5a0662a
Joao-Aquino25 <joaoovcarvalhoo@gmail.com>
2025-06-20 09:59:36 -0300

    feat: adicionar função de remover e concluir tarefas com localStorage

--- scripts.js
+++ scripts.js
@@ -7,33 +7,69 @@ let minhaListaDeItens = []
 
 
 function adicionarNovaTarefa() {
-  minhaListaDeItens.push(input.value);
+
+  if (input.value.trim() === '') {
+    alert("Por favor, insira uma tarefa antes de adicionar!");
+    return;
+  }
+
+  minhaListaDeItens.push({
+    tarefa: input.value,
+    concluida: false
+  })
 
   input.value = ''
 
   mostrarTarefas()
 }
 
+
+
 function mostrarTarefas() {
 
   let novaLi = ''
 
-  minhaListaDeItens.forEach(tarefa => {
+  minhaListaDeItens.forEach((item, index) => {
 
     novaLi = novaLi + `
 
-    <li class="task-item">
-          <img src="img/checked.png" alt="check">
-          <p>${tarefa}.</p>
-          <img src="img/trash.png" alt="uncheck">
-      </li>
+        <li class="task-item ${item.concluida && 'done'}">
+            <img src="img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${index})">
+            <p>${item.tarefa}</p>
+            <img src="img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarTarefa(${index})">
+        </li>
 
       `
   })
 
   listaCompleta.innerHTML = novaLi
+
+  localStorage.setItem('lista', JSON.stringify(minhaListaDeItens))
+
 }
 
+function concluirTarefa(index) {
+  minhaListaDeItens[index].concluida = !minhaListaDeItens[index].concluida
+
+  mostrarTarefas()
+}
+
+function deletarTarefa(index) {
+  minhaListaDeItens.splice(index, 1)
+  mostrarTarefas()
+}
+
+function carregarTarefas() {
+  const tarefasLocalStorage = localStorage.getItem('lista')
+
+  if (tarefasLocalStorage) {
+    minhaListaDeItens = JSON.parse(tarefasLocalStorage)
+  }
+
+  mostrarTarefas()
+}
+
+carregarTarefas()
 
 button.addEventListener('click', adicionarNovaTarefa)
 
